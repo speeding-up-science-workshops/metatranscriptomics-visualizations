68f10e69bc849145e02927f7ca78667c2dc34531
AstrobioMike <michael.lee0517@gmail.com>
2019-05-09 10:40:06 -0700

    mod

---
+++ install.R
@@ -0,0 +1,18 @@
+install.packages("vegan")
+install.packages("tidyr")
+install.packages("viridis")
+install.packages("reshape")
+
+install.packages("devtools")
+devtools::install_github("benjjneb/dada2")
+devtools::install_github("benjjneb/decontam")
+devtools::install_version("mvtnorm", version = "1.0-8", repos = "http://cran.us.r-project.org")
+devtools::install_version("fpc", version = "2.1-11.1", repos = "http://cran.us.r-project.org")
+
+
+source("https://bioconductor.org/biocLite.R")
+biocLite(suppressUpdates = FALSE)
+biocLite("phyloseq")
+biocLite("DESeq2")
+biocLite("ShortRead")
+biocLite("dendextend")
