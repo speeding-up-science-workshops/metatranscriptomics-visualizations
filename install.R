e5aba105dc4f3297cac55193bcaef6288a23e7a6
Mike Lee <michael.lee0517@gmail.com>
2019-10-24 15:53:19 -0700

    Update install.R

--- install.R
+++ install.R
@@ -1,4 +1,8 @@
-pkgs = c("knitr", "rmarkdown", "ggplot2", "ggpubr", "reshape2","cowplot","superheat","plyr","dplyr", "vegan", "reshape", "devtools", "pheatmap", "BiocManager")
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install()
+
+pkgs = c("knitr", "rmarkdown", "ggplot2", "ggpubr", "reshape2","cowplot","superheat","plyr","dplyr", "vegan", "reshape", "devtools", "pheatmap")
 ncores = parallel::detectCores()
 
 install.packages(pkgs, Ncpus = ncores)
