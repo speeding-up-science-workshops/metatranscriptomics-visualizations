5002b42ef4d9fe6cf3294ba909a6a22846cdf15f
Mike Lee <michael.lee0517@gmail.com>
2019-10-24 10:32:48 -0700

    Update install.R

--- install.R
+++ install.R
@@ -1,8 +1,7 @@
-pkgs = c("knitr", "rmarkdown", "ggplot2", "ggpubr", "reshape2","cowplot","superheat","plyr","dplyr", "vegan", "reshape", "devtools", "pheatmap")
+pkgs = c("knitr", "rmarkdown", "ggplot2", "ggpubr", "reshape2","cowplot","superheat","plyr","dplyr", "vegan", "reshape", "devtools", "pheatmap", "BiocManager")
 ncores = parallel::detectCores()
 
 install.packages(pkgs, Ncpus = ncores)
-
 
 devtools::install_github("benjjneb/dada2")
 devtools::install_github("benjjneb/decontam")
